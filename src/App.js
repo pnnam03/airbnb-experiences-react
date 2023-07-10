d63890dc2d753c436b4a87c3d29020fcaad68dfb
unknown <pnnam0330@gmail.com>
2023-07-10 15:24:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import vector from './img/Vector.svg'
+import group77 from './img/Group 77.svg'
+import image12 from './img/image 12.svg'
+import wedding_photo from './img/wedding-photography 1.svg'
+import mountain_bike from './img/mountain-bike 1.svg'
+import star from './img/Star 1.svg'
+import ellipse from './img/Ellipse 6.svg'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="rectangle13">
+        <img src={vector} alt=""></img>
+      </div>
+
+      <div>
+        <img className="all-img" src={group77} alt=""></img>
+      </div>
+
+      <div className="title">
+        Online Experiences
+      </div>
+
+      <div className="content">
+      Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+      </div>
+
+      <div className="lower">
+        <img src={image12} alt=""></img>
+        <div>
+          SOLD OUT
+        </div>
+
+        <div>
+          <img src={star} className="star" alt=""></img>
+          <div className="info">
+            <div className="rating">5.0</div>
+            <div>(6)</div>
+            <img src={ellipse} className="ellipse" alt=""></img>
+            <div>USA</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src={wedding_photo} alt=""></img>
+        <div>
+          ONLINE
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+      <div>
+        <img src={mountain_bike} alt=""></img>
+      </div>
     </div>
   );
 }
